0dbc4a9a81e39b85aad3e2b42eeecd1c57d5c73a
zh158 <320637585@qq.com>
2026-05-15 01:39:04 +0800

    fix: 自动修复 null-user 题目，Railway 无需手动命令

--- back/apps/questions/apps.py
+++ back/apps/questions/apps.py
@@ -5,3 +5,14 @@ class QuestionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.questions'
     verbose_name = '题目管理'
+
+    def ready(self):
+        from .models import Question
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        null_count = Question.objects.filter(user__isnull=True).count()
+        if null_count > 0:
+            admin = User.objects.filter(is_superuser=True).first() or User.objects.first()
+            if admin:
+                Question.objects.filter(user__isnull=True).update(user=admin)
