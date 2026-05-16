c6d4c7b2b86d90395c7b55ebad064af48ef71c88
zh158 <320637585@qq.com>
2026-05-16 09:42:01 +0800

    shorten JWT refresh token to 12 hours

--- back/config/settings.py
+++ back/config/settings.py
@@ -122,7 +122,7 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(hours=12),
 }
 
 # Custom User Model
